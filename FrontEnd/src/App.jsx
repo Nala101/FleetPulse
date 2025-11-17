3490f33fb9ebbb9215956db94d917ad2e4028671
Nathan La <nala@uci.edu>
2025-11-17 15:32:57 -0800

    copied in some boiler plate code to start the project

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>title</h1>
+        <p>text</p>
+      </header>
+    </div>
+  )
+}
