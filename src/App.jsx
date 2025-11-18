bc6006dabc666a89216ac594d6ad3117a602bc34
Nathan La <nala@uci.edu>
2025-11-17 16:03:41 -0800

    added tailwind CSS files to the project and imported the modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./App.css";
 
 export default function App() {
   return (
@@ -6,7 +7,10 @@ export default function App() {
       <header className="app-header">
         <h1>title</h1>
         <p>text</p>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Click Me
+        </button>
       </header>
     </div>
-  )
+  );
 }
