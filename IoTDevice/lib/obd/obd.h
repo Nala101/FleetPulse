1cb5fc02161c849fb7d5054ba9934663fe9869cd
NathanBarfield <nbarfiel@uci.edu>
2025-11-22 18:38:06 -0800

    Added OBDII reading code.

---
+++ IoTDevice/lib/obd/obd.h
@@ -0,0 +1,33 @@
+#ifndef _OBD_H
+#define _OBD_H
+
+#define OBD_BAUD 9600
+#define OBD_MSG_LEN 20
+#define AFR 14.7f
+#define DENSITY_G_PER_GAL 7480.0f
+
+
+struct OBDData {
+    const float mph;
+    const float milesTraveled;
+    const float rpm;
+    const float fuelPercent;
+    const float galPerHour;
+    const float galUsed;
+    const float engineTemp;
+    OBDData(const float& mph, const float& milesTraveled, const float& rpm, const float& fuelPercent, const float& galPerHour,
+            const float& galUsed, const float& engineTemp) : mph(mph), milesTraveled(milesTraveled), rpm(rpm), fuelPercent(fuelPercent),
+            galPerHour(galPerHour), galUsed(galUsed), engineTemp(engineTemp) {}
+};
+
+char* trimResponse(char* buf);
+
+void clearOBDBuffer();
+
+char* getResponse(const char* command, char* buf);
+
+void setupOBD();
+
+const OBDData requestOBDData();
+
+#endif /* _GPS_H */
--- IoTDevice/src/main.cpp
+++ IoTDevice/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "gps.h"
 #include "dht.h"
+#include "obd.h"
 
 #define TELEMETRY_INTERVAL 3000
 unsigned long lastTelemetryTime = 0;            // Used to track the timestamp of the last time telemetry data was sent to the Azure IoT Hub
@@ -9,6 +10,7 @@ void setup() {
     Serial.begin(9600);
     setupGPS();
 	setupDHT();
+	setupOBD();
 }
 
 void loop() {
@@ -23,6 +25,14 @@ void loop() {
 		Serial.println("------DHT------");
 		Serial.println(dhtData.temperature);
 		Serial.println(dhtData.humidity);
+		const OBDData obdData = requestOBDData();
+		Serial.println(obdData.mph, 2);
+		Serial.println(obdData.milesTraveled, 10);
+		Serial.println(obdData.rpm, 2);
+		Serial.println(obdData.fuelPercent, 2);
+		Serial.println(obdData.galPerHour, 2);
+		Serial.println(obdData.galUsed, 10);
+		Serial.println(obdData.engineTemp, 2);
 		lastTelemetryTime = millis();       // Records the current time as the timestamp of the last telemetry transmission
 	}
 }
