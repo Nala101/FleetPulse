896739aa543646e4fa575ecf934c8cc2d9071fad
NathanBarfield <nbarfiel@uci.edu>
2025-12-12 23:22:28 -0800

    Added comments to the OBD sensor code.

--- IoTDevice/lib/obd/obd.cpp
+++ IoTDevice/lib/obd/obd.cpp
@@ -1,13 +1,17 @@
-#include "obd.h"
-#include "dht.h"
-#include <Arduino.h>
+#include "obd.h"										// Includes the corresponding header file
+#include "dht.h"										// Includes the DHT header file for the function celsiusToFahrenheit
+#include <Arduino.h>									// Includes the Arduino library for standard Arduino functions
 
+// Declares the timestamp of the last MPH and GPH readings
 long lastMPHReadingTimestamp;
 long lastGPHReadingTimestamp;
 
+// Setups the OBD serial on UART1
 HardwareSerial obdSerial(1);
 
+// Trims the beginning of the OBD response message stored in buf as to remove unnecessary characters and confirmation messages
 char* trimResponse(char* buf) {
+	// Loops through the messages that contain the confirmation and the actual data message
 	for (int i = 0; i < 2; ++i) {
 		for (; *buf != ' '; ++buf) {
 			if (*buf == '\0') {
@@ -22,6 +26,7 @@ char* trimResponse(char* buf) {
 	return buf;
 }
 
+// Clears the OBD buffer
 void clearOBDBuffer() {
 	while (obdSerial.available() > 0) {
 		obdSerial.read();
@@ -29,6 +34,7 @@ void clearOBDBuffer() {
 	}
 }
 
+// Sends the command to the OBD sensor, receives the reply from the sensor, formats the response, and returns it
 char* getResponse(const char* command, char* buf) {
     clearOBDBuffer();
     obdSerial.println(command);
@@ -36,10 +42,13 @@ char* getResponse(const char* command, char* buf) {
     int index = 0;
     long start = millis();
 
+	// Repeats while the communication has not timed out
     while (millis() - start < 1000) {
+		// Reads data from the serial buffer
         if (obdSerial.available()) {
             char c = obdSerial.read();
 
+			// If the message starts with a '>', it contains the actual data
             if (c == '>') {
                 buf[index] = '\0';
 				char* response = trimResponse(buf);
@@ -60,6 +69,7 @@ char* getResponse(const char* command, char* buf) {
     return response;
 }
 
+// Setups the MPH and GPH timestamps, the OBD serial, and sends the starting commands to the OBD sensor
 void setupOBD() {
 	lastMPHReadingTimestamp = millis();
 	lastGPHReadingTimestamp = millis();
@@ -72,6 +82,7 @@ void setupOBD() {
 	}
 }
 
+// Requests data from the OBD sensor and returns it as a OBDData object
 const OBDData requestOBDData() {
 	char buf[OBD_MSG_LEN];
 	char* response;
@@ -83,51 +94,62 @@ const OBDData requestOBDData() {
 	float galUsed = 0;
 	float engineTemp = 0;
 
+	// Asks for the current mph from the OBD sensor
 	response = getResponse("010D", buf);
 	if (!response) {
 		Serial.println("Error reading data");
-
 	}
 	else {
+		// Converts the response into mph
 		mph = strtol(&response[0], nullptr, 16) * 0.621371;
+		// Calculates the miles traveled using the mph and the time since the last reading for mph
 		milesTraveled = mph * ((millis() - lastMPHReadingTimestamp) / 3600000.0f);
 		lastMPHReadingTimestamp = millis();
 	}
 
+	// Asks for the current rpm from the OBD sensor
 	response = getResponse("010C", buf);
 	if (!response) {
 		Serial.println("Error reading data");
 	}
 	else {
+		// Converts the response into rpm
 		rpm = ((strtol(&response[0], nullptr, 16) * 256) + strtol(&response[3], nullptr, 16)) / 4;
 	}
 
+	// Asks for the current fuel percentage from the OBD sensor
 	response = getResponse("012F", buf);
 	if (!response) {
 		Serial.println("Error reading data");
 	}
 	else {
+		// Converts the response into fuel percentage
 		fuelPercent = (strtol(&response[0], nullptr, 16) * 100.0f) / 255.0f;
 	}
 
+	// Asks for the current maf from the OBD sensor
 	response = getResponse("0110", buf);
 	if (!response) {
 		Serial.println("Error reading data");
 	}
 	else {
+		// Converts the response into gallons per hour and gallons used since the last reading for gallons used
 		float mafGPS = (((strtol(&response[0], nullptr, 16) * 256) + strtol(&response[3], nullptr, 16)) / 100.0f);
 		galPerHour = (mafGPS / AFR) * 3600.0f / DENSITY_G_PER_GAL;
 		galUsed = galPerHour * ((millis() - lastGPHReadingTimestamp) / 3600000.0f);
 		lastGPHReadingTimestamp = millis();
 	}
 
+	// Asks for the current engine temperature from the OBD sensor
 	response = getResponse("0105", buf);
 	if (!response) {
 		Serial.println("Error reading data");
 	}
 	else {
+		// Converts the response into engine temperature in fahrenheit
 		engineTemp = celsiusToFahrenheit(strtol(&response[0], nullptr, 16) - 40);
 	}
 
+	// Returns the collected data as an OBDData object
 	return OBDData(mph, milesTraveled, rpm, fuelPercent, galPerHour, galUsed, engineTemp);
 }
--- IoTDevice/lib/obd/obd.h
+++ IoTDevice/lib/obd/obd.h
@@ -1,13 +1,17 @@
 #ifndef _OBD_H
 #define _OBD_H
 
+// Defines the pins and baud rate used by the OBD
 #define OBD_RX_PIN 26
 #define OBD_TX_PIN 25
 #define OBD_BAUD 9600
+
+// Defines the OBD message length and constants used in data calculations
 #define OBD_MSG_LEN 20
 #define AFR 14.7f
 #define DENSITY_G_PER_GAL 2820.0f
 
+// Declares a struct used to store data from the OBD sensor
 struct OBDData {
     const float mph;
     const float milesTraveled;
@@ -21,14 +25,19 @@ struct OBDData {
             galPerHour(galPerHour), galUsed(galUsed), engineTemp(engineTemp) {}
 };
 
+// Trims the beginning of the OBD response message stored in buf as to remove unnecessary characters and confirmation messages
 char* trimResponse(char* buf);
 
+// Clears the OBD buffer
 void clearOBDBuffer();
 
+// Sends the command to the OBD sensor, receives the reply from the sensor, formats the response, and returns it
 char* getResponse(const char* command, char* buf);
 
+// Setups the MPH and GPH timestamps, the OBD serial, and sends the starting commands to the OBD sensor
 void setupOBD();
 
+// Requests data from the OBD sensor and returns it as a OBDData object
 const OBDData requestOBDData();
 
 #endif /* _GPS_H */
