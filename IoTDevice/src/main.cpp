800dfc23bbf2adc51e8e4c640cbfeed9fc6c3cc0
NathanBarfield <nbarfiel@uci.edu>
2025-12-12 23:32:47 -0800

    Added comments to main.cpp

--- IoTDevice/src/main.cpp
+++ IoTDevice/src/main.cpp
@@ -1,19 +1,22 @@
-#include <Arduino.h>
+#include <Arduino.h>							// Includes the Arduino library for standard Arduino functions
 #include <HTTPClient.h>                         // Includes the HTTP client library for functions used to send HTTP requests
 #include <WiFi.h>                               // Includes the WiFi library used to handle WiFi connections for the ESP32
 #include <WiFiClient.h>                         // Includes the WiFi client library used to provide a TCP connection client over WiFi
 #include "Wire.h"                               // Includes the Wire library for I2C communication
 #include "DHT20.h"                              // Includes the DHT20 library for interacting with the humidity/temperature sensor
-#include <ArduinoJson.h>                        // Incudess the Arduino JSON library for creating JSON payloads
-#include <constants.h>
-#include "gps.h"
-#include "dht.h"
-#include "obd.h"
+#include <ArduinoJson.h>                        // Includes the Arduino JSON library for creating JSON payloads
+#include <constants.h>							// Includes the constants file that contains the Wi-Fi SSID, Wi-Fi password, SAS token, ROOT CA, IoT hub name, IoT device name, and IoT hub endpoint url
+#include "gps.h"								// Includes the library used to interact with the GPS sensor
+#include "dht.h"								// Includes the library used to interact with the DHT sensor
+#include "obd.h"								// Includes the library used to interact with the OBD sensor
 
+// Sets the telemetry interval to 3 seconds
 #define TELEMETRY_INTERVAL 3000
 unsigned long lastTelemetryTime = 0;            // Used to track the timestamp of the last time telemetry data was sent to the Azure IoT Hub
 
+// Setups the IoT device
 void setup() {
+	// Starts the serial console and setups each sensor
     Serial.begin(9600);
     setupGPS();
 	setupDHT();
@@ -30,9 +33,10 @@ void setup() {
 
 	configTime(0, 0, "pool.ntp.org", "time.nist.gov");
 
+	// Time sync code from chatgpt
 	Serial.println("Waiting for NTP time sync...");
 	time_t now = time(nullptr);
-	while (now < 1700000000) {   // ~2023 in Unix time
+	while (now < 1700000000) {
 		delay(200);
 		now = time(nullptr);
 	}
@@ -46,9 +50,12 @@ void setup() {
     Serial.println(WiFi.macAddress());
 }
 
+// The main loop function that sends telemetry data to the IoT Hub
 void loop() {
+	// If it has been longer than the telemetry interval, send another telemetry message
 	if (millis() - lastTelemetryTime >= TELEMETRY_INTERVAL) {
-		ArduinoJson::JsonDocument doc;      // Creates a JSON payload
+		ArduinoJson::JsonDocument doc;
+		// Gets the data from the GPS, prints each data value to the console, and stores each data value into the JSON payload
 		Serial.println("------GPS------");
 		const GPSData gpsData = requestGPSData();
 		Serial.println(gpsData.fix);
@@ -60,6 +67,7 @@ void loop() {
 		Serial.println(gpsData.longitude, 14);
 		doc["longitude"] = gpsData.longitude;
 
+		// Gets the data from the DHT, prints each data value to the console, and stores each data value into the JSON payload
 		Serial.println("------DHT------");
 		const DHTData dhtData = requestDHTData();
 		Serial.println(dhtData.temperature);
@@ -67,6 +75,7 @@ void loop() {
 		Serial.println(dhtData.humidity);
 		doc["cabinHumidity"] = dhtData.humidity;
 
+		// Gets the data from the OBD, prints each data value to the console, and stores each data value into the JSON payload
 		Serial.println("------OBD------");
 		const OBDData obdData = requestOBDData();
 		Serial.printf("%.2f mph\n", obdData.mph);
@@ -84,22 +93,29 @@ void loop() {
 		Serial.printf("%.2f F\n", obdData.engineTemp);
 		doc["engineTemp"] = obdData.engineTemp;
 
+		// Flattens the JSON payload into buffer
 		char buffer[512];
         serializeJson(doc, buffer, sizeof(buffer));
 
+		// Prints out the JSON payload
 		Serial.println(buffer);
 
+		// Sets the ROOT CA certificate
         WiFiClientSecure client;
         client.setCACert(ROOT_CA);
 
+		// Creates the HTTP object and header
 		HTTPClient http;
         http.begin(client, url);
         http.addHeader("Content-Type", "application/json");
         http.addHeader("Authorization", SAS_TOKEN);
+		// Sends the http object to the IoT hub
         int httpCode = http.POST(buffer);
+		// If the http object was successfully received, print out the sent message
         if (httpCode == 204) {
             Serial.println("Telemetry sent: " + String(buffer));
         }
+		// Otherwise, print out the error code
         else {
             Serial.println("Failed to send telemetry. HTTP code: " + String(httpCode));
         }
